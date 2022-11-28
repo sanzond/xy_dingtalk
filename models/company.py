b76a736940e64afdd47d91fc6c1d888f6edbebe9
ChaoqiYin <305111632@qq.com>
2022-11-28 11:16:59 +0800

    fix: app_ids -> ding_app_ids

--- models/company.py
+++ models/company.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class Company(models.Model):
     _inherit = 'res.company'
 
-    app_ids = fields.One2many('dingtalk.app', 'company_id', string='Apps')
+    ding_app_ids = fields.One2many('dingtalk.app', 'company_id', string='Apps')
